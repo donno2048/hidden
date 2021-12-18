7754b147be915dc3d6433a2844a11130dd1e5c2a
donno2048 <just4now666666@gmail.com>
2021-12-18 18:10:33 +0200

    use squares

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ img = Image.new("RGB", source.size)
 pix = img.load()
 for i in range(source.size[0]):
     for j in range(source.size[1]):
-        if (i // max(1, source.size[0] // (1 << 8)) + j // max(1, source.size[1] // (1 << 8))) % 2: # resolution of no more than 1<<8
+        scale = max(1, min(source.size) // (1 << 8))
+        if (i // scale + j // scale) % 2: # resolution of no more than 1<<8
             pix[i, j] = (0xFF,) * 3
 Image.blend(source, img.convert("RGBA"), .9).save(path.join(path.dirname(args.file), "hidden_" + path.basename(args.file)), "PNG")
